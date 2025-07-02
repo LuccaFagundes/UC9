7603e7aabf05c6f02134a2d9328c00f321e96dad
Lucca Fagundes <luccapfagundes@gmail.com>
2025-07-02 19:22:49 -0300

    Delete Aula 2 - ( 07-01-2025 ) incomplete title message

--- Aula 2 - ( 07-01-2025 )/alunos.sql
+++
@@ -1,7 +0,0 @@
-/*
--- Query: 
--- Date: 2025-07-01 21:32
-*/
-INSERT INTO `` (`id_aluno`,`nome_aluno`,`idade`,`telefone`) VALUES (1,'Lucas','30~',999999191);
-INSERT INTO `` (`id_aluno`,`nome_aluno`,`idade`,`telefone`) VALUES (2,'Gabiss','27~',901239013);
-INSERT INTO `` (`id_aluno`,`nome_aluno`,`idade`,`telefone`) VALUES (3,'Lucca','21~',931349013);
