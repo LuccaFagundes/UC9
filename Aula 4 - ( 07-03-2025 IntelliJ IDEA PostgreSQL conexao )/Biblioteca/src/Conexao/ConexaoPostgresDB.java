5c8ce1dc2c6f9041c182ba6d3142ee705a675490
Lucca Fagundes <luccapfagundes@gmail.com>
2025-07-03 21:52:01 -0300

    Add files via upload

---
+++ Aula 4 - ( 07-03-2025 IntelliJ IDEA PostgreSQL conexao )/Biblioteca/src/Conexao/ConexaoPostgresDB.java
@@ -0,0 +1,115 @@
+package Conexao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class ConexaoPostgresDB {
+    private static final String URL = "jdbc:postgresql://localhost:5432/biblioteca";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "root";
+
+    public static Connection conectar() {
+        Connection conexao = null;
+        try {
+            conexao = DriverManager.getConnection(URL,USER,PASSWORD);
+            System.out.println("Conexão com o banco de dados estabelecida com sucesso!");
+        } catch (SQLException e) {
+            System.err.println("Erro ao conectar com o banco: " + e.getMessage());
+        } return conexao;
+    }
+
+    public static void fecharConexao(Connection conexao) {
+        if (conexao != null) {
+            try {
+                conexao.close();
+                System.out.println("Conexão com o banco de dados fechada!");
+            } catch (SQLException e) {
+                System.err.println("Erro ao fechar a conexão com o banco: " + e.getMessage());
+            }
+        }
+    }
+
+    public static void setAluno(String nome,int idade, String telefone) {
+        String sql = "INSERT INTO alunos (nome_aluno,idade_aluno,contato_aluno) VALUES (?,?,?)";
+        Connection conexao = null;
+        PreparedStatement stmt = null;
+        try {
+            conexao = ConexaoDB.conectar();
+            if (conexao != null) {
+                stmt = conexao.prepareStatement(sql);
+                stmt.setString(1, nome);
+                stmt.setInt(2, idade);
+                stmt.setString(3, telefone);
+                int linhasAfetadas = stmt.executeUpdate();
+                if (linhasAfetadas > 0) {
+                    System.out.println("Aluno " + nome + " inserido no BD com sucesso!");
+                }
+            }
+        } catch (SQLException e) {
+            System.err.println("Erro ao inserir aluno no PostgreSQL: " + e.getMessage());
+        } finally {
+            try {
+                if (stmt != null) stmt.close();
+                if (conexao != null) fecharConexao(conexao);
+            } catch (SQLException e) {
+                System.err.println("Erro ao fechar recursos após inserção " + e.getMessage());
+            }
+        }
+    }
+
+    public static void getAlunos(){
+        String sql = "SELECT * FROM aluno ORDER BY id_aluno";
+        Connection conexao = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        try {
+            conexao = ConexaoDB.conectar();
+            if (conexao != null) {
+                stmt = conexao.prepareStatement(sql);
+                rs = stmt.executeQuery();
+                System.out.println("\n--- Aluno(s) Cadastrados no BD ---");
+                boolean encontrouAluno = false;
+                while (rs.next()) {
+                    encontrouAluno = true;
+                    int id = rs.getInt("id_aluno");
+                    String nome = rs.getString("nome_aluno");
+                    int idade = rs.getInt("idade_aluno");
+                    String telefone = rs.getString("contato_aluno");
+                    System.out.println("ID: " + id + ", Nome: " + nome + ", Idade: " + idade + ", Telefone: " + telefone);
+                }
+                if(!encontrouAluno){
+                    System.out.println("Nenhum aluno encontrado!");
+                }
+                System.out.println("------------------------------\n");
+            }
+        } catch ( SQLException e){
+            System.out.println("Erro ao consultar alunos: " + e.getMessage());
+        } finally {
+            try {
+                if (rs != null) rs.close();
+                if(stmt != null) stmt.close();
+                if (conexao != null) fecharConexao(conexao);
+            } catch (SQLException e) {
+                System.out.println("Erro ao fechar recursos após consulta: " + e.getMessage());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Connection testeConexao = ConexaoPostgresDB.conectar();
+        if(testeConexao != null) {
+            ConexaoPostgresDB.fecharConexao(testeConexao);
+        }
+
+        System.out.println("\n--- Realizando Inserções ---");
+        setAluno("João",17,"9319314");
+        setAluno("Jorge",20,"9431943");
+
+        System.out.println("\n--- Realizando Consulta ---");
+        getAlunos();
+    }
+
+}
