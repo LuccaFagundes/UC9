6570ad632f482a2473f5b6971ffaff8a6e81ca88
Lucca Fagundes <luccapfagundes@gmail.com>
2025-07-10 19:17:11 -0300

    Add files via upload

---
+++ Aula 6 - ( 07-09-2025 Jurassic World )/JurassicWorld/src/Conexao/ConexaoPark.java
@@ -0,0 +1,51 @@
+package Conexao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class ConexaoPark {
+    private static final String URL = "jdbc:postgresql://localhost:5432/JurassicWorld";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "root";
+
+    public static Connection conectar() {
+        Connection conexao = null;
+        try {
+            conexao = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Conexão com o banco de dados estabelecida com sucesso!");
+        } catch (SQLException e) {
+            System.err.println("Erro ao conectar com o banco: " + e.getMessage());
+        }
+        return conexao;
+    }
+
+    public static void fecharConexao(Connection conexao) {
+        if (conexao != null) {
+            try {
+                conexao.close();
+                System.out.println("Conexão com o banco de dados fechada!");
+            } catch (SQLException e) {
+                System.err.println("Erro ao fechar a conexão com o banco: " + e.getMessage());
+            }
+        }
+    }
+
+    public static void setDinossauro(String nome, String especie, String dieta, int idade_estimada_anos, int idade_do_dinossauro, String status_cercado) {
+        String sql = "INSERT INTO Dinossauros (nome,especie.dieta,idade_estimada_anos,idade_do_dinossauro,status_cercado) VALUES (?,?,?,?,?) ";
+        Connection conexao = null;
+        PreparedStatement preparedStatement = null;
+
+        try {
+            conexao = ConexaoPark.conectar();
+            if (conexao != null){
+
+            }
+        } catch (SQLException e){
+            System.err.println("Erro ao conectar com o banco: " + e.getMessage());
+        }
+
+    }
+}
