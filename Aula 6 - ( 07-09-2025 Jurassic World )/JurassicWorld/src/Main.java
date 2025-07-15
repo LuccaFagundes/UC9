973f4a5307aeb72ed7a32efde601ca28eca75910
Lucca Fagundes <luccapfagundes@gmail.com>
2025-07-15 19:34:16 -0300

    Update Main.java

--- Aula 6 - ( 07-09-2025 Jurassic World )/JurassicWorld/src/Main.java
+++ Aula 6 - ( 07-09-2025 Jurassic World )/JurassicWorld/src/Main.java
@@ -11,13 +11,11 @@ public class Main {
         System.out.println("\n--- Testando Inserção ---");
         Dino mosassauro = new Dino("Mosassauro", "Mosasauridae", "Herbívoro", 68, 12, "Seguro");
         Dino quetzalcoatlus = new Dino("Quetzalcoatlus", "Quetzalcoatlus northropi", "Piscívoro", 68, 7, "Seguro");
-        Dino dinossauroParaVirarVelociraptor = new Dino("Fakeossauro", "Mentira Mentirais", "Herbívoro", 69, 500, "Seguro");
-        Dino dinossauroParaSerDeletar = new Dino("Inexistencia", "Voidicus Inexistencius", "Herbívoro", 90, 420, "Seguro");
+        Dino velociraptor = new Dino("Velociraptors", "Velociraptor mongoliensis", "Carnivor", 75, 5, "Contido");
 
         dinoDAO.setDinossauro(mosassauro);
         dinoDAO.setDinossauro(quetzalcoatlus);
-        dinoDAO.setDinossauro(dinossauroParaVirarVelociraptor);
-        dinoDAO.setDinossauro(dinossauroParaSerDeletar);
+        dinoDAO.setDinossauro(velociraptor);
 
 
         System.out.println("\n--- Testando Listagem ---");
@@ -25,12 +23,10 @@ public class Main {
 
 
         System.out.println("\n--- Testando Atualização ---");
-        Dino velociraptor = new Dino(3,"Velociraptor", "Velociraptor mongoliensis", "Carnívoro", 75, 5, "contido");
-        dinoDAO.updateDinossauro(velociraptor);
 
 
         System.out.println("\n--- Testando remoção ---");
-        dinoDAO.removerDinossauro(4);
+
 
         dinoDAO.getDinossauro();
 
